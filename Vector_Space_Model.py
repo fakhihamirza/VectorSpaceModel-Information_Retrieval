5f56458f5cfd5a8116230522e460306674de0beb
unknown <mfakhiha@gmail.com>
2021-05-02 15:58:49 +0500

    query vector created

--- Vector_Space_Model.py
+++ Vector_Space_Model.py
@@ -15,6 +15,8 @@ stemmed_tokens=[]
 Index = {}
 ps = PorterStemmer()
 total_documents = 50
+q_vector = {}
+all_words =[]
 
 def Pre_processing():
     #for all documents in corpus-open file and read
@@ -31,6 +33,7 @@ def Pre_processing():
         tokens = nltk.word_tokenize(content)
         #Stemming
         stemmed_tokens=[ps.stem(word) for word in tokens]
+        all_words.extend(stemmed_tokens)
         #Creating Index
         word_position = 0
         for word in stemmed_tokens:
@@ -48,9 +51,8 @@ def Pre_processing():
                     if Index[word]['tf'][doc_num -1] == 0 :  #found in new doc
                         Index[word]['df'] = Index[word]['df'] + 1
                     Index[word]['tf'][doc_num -1 ] = Index[word]['tf'][doc_num -1 ] +1
-                Index[word]['idf'] = math.log(total_documents/(Index[word]['df']))     #formula is log(N/df) not log(df/N)
+                Index[word]['idf'] = math.log10(total_documents/(Index[word]['df']))     #formula is log(N/df) not log(df/N)
                 Index[word]['tf-idf'][doc_num -1 ] = Index[word]['tf'][doc_num -1] * Index[word]['idf']
-
 
 # Writing indexes to file
 def write_to_file():
@@ -58,5 +60,37 @@ def write_to_file():
     f.write( str(Index) )
     f.close()
 
+    f = open("Query_vector.txt","w")
+    f.write( str(q_vector) )
+    f.close()
+
+def query_vector(query):
+    query=query.lower() 
+    regex = re.compile('[^a-zA-Z0-9\s]')
+    query = re.sub(regex,' ',query)
+    regex = re.compile('[-]')
+    query = re.sub(regex,' ',query)
+    q_list = nltk.word_tokenize(query)  
+
+    q_tokens=[ps.stem(word) for word in q_list]
+
+    print(q_tokens)
+    
+    for word in all_words:
+        if word not in stopwords:
+            if word not in q_vector:
+                q_vector[word] = 0
+            else:                
+                if word not in q_tokens:
+                    q_vector[word] =0
+                else:
+                    q_vector[word] = (q_tokens.count(word) * Index[word]['idf'])  # multiplying term freq in query * idf of word
+
+
+
+
 Pre_processing()
+q = input()
+print(stemmed_tokens)
+query_vector(q)
 write_to_file()
